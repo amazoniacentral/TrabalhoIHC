1feefbc1e197b3571be77ff2607e5dc46c10758b
amazoniacentral\francisco <radioamazoniacentral@gmail.com>
2017-05-25 04:26:17 -0400

    Começei a fazer a parte de programação. Nessa etapa fiz a função para abrir a câmera para ler o QR Code e a forma de pagamento. falta fazer todo o layout e a lista de pedidos e também os pedidos selecionados.

---
+++ Scripts/Site.js
@@ -0,0 +1,38 @@
+﻿function AbrirCamera()
+{
+    ///VERIFICAR SE JÁ ESTÁ LOGADO (SE JÁ LEU O QR CODE)
+    var s = EstaLogado();
+    if (s == 'sim') {
+        window.location.href = "Pedido.html";
+    }
+    else {
+        ///ABRIR A CÂMERA PARA LER O QRCODE
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function (stream) {
+            var video = document.querySelector('video');
+            video.srcObject = stream;
+            video.play();
+        });
+    }
+}
+
+function EstaLogado()
+{
+    ///VERIFICAR SE JÁ ESTÁ LOGADO (SE JÁ LEU O QR CODE) E RETORNA A RESPOSTA
+    var s = sessionStorage.getItem('logado');
+    if (s=='sim') {
+        return 'sim';
+    }
+    return 'não';
+}
+
+function GravarLogin() {
+    ///GRAVAR LEITURA DO AR CODE
+    sessionStorage.setItem('logado', 'sim');
+}
+
+function Sair() {
+    ///SAIR (REMOVER SESSÃO)
+    sessionStorage.removeItem('logado');
+    sessionStorage.removeItem('forma');
+    window.location.href = "Index.html";
+}
